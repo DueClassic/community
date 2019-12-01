46fe6afff4427270872414e5793c387d76c4a95e
DueClassic <alfred1219784057@gmail.com>
2019-12-01 18:09:32 +0800

    add check cookies

--- src/main/java/com/cc/community/controller/IndexController.java
+++ src/main/java/com/cc/community/controller/IndexController.java
@@ -1,15 +1,34 @@
 package com.cc.community.controller;
 
+import com.cc.community.mapper.UserMapper;
+import com.cc.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by xiaomi on 2019/11/30.
  */
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies=request.getCookies();
+        for(Cookie cookie:cookies){
+            if(("token").equals(cookie.getName())){
+                String token=cookie.getValue();
+                User user=userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
--- src/main/java/com/cc/community/mapper/UserMapper.java
+++ src/main/java/com/cc/community/mapper/UserMapper.java
@@ -3,12 +3,17 @@ package com.cc.community.mapper;
 import com.cc.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by xiaomi on 2019/12/1.
  */
+@Component
 @Mapper
 public interface UserMapper {
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+    @Select("select * from user where token=#{token}")
+    User findByToken(String token);
 }
